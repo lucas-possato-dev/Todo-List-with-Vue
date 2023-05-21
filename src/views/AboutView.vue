5e0f6d196e407a4199becdfcad939660b45352c7
lucas-possato-dev <lucaspstinho@gmail.com>
2023-05-21 14:02:16 -0300

    First commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="about">
+    <h1>About</h1>
+    <p>
+      Welcome to my very first Vue.js application, a simple and intuitive Todo
+      app! I am excited to share this project with you and showcase my journey
+      in learning and implementing Vue.js. The Idea for this project came from
+      John Komarnicki's crash course on Vue 3, available on youtube.
+    </p>
+  </main>
+</template>
+
+<style lang="scss">
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+}
+</style>
